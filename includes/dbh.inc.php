87fa3bac8025d85ea7c4577d11ab5d7aaa5bfc60
gananath-oss <ganaya2k17@gmail.com>
2024-04-23 19:05:29 +0500

    configuration
    - configuration and secure sessions
    - connect to the local host

---
+++ includes/dbh.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+    $host = "localhost";
+    $dbname = "myfirstdb";
+    $dbusername = "root";
+    $dbpassword = "root"; 
+
+    try {
+        $pdo = new PDO("mysql:host=$host;dbnane=$dbname", $dbusername, $dbpassword);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die("Connection failed: " . $e->getMessage())
+    }
